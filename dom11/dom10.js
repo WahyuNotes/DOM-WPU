86f044fee009f0e5260621c525150070c448d5da
wramadhan <wahyuramadhan0549@gmail.com>
2022-08-24 11:48:00 +0700

    tambah event bubbling

--- dom11/dom10.js
+++ dom11/dom10.js
@@ -11,7 +11,7 @@ hapus.addEventListener("click", (e) => {
 */
 
 //buat DOM Transversal
-const close = document.querySelectorAll(".close");
+// const close = document.querySelectorAll(".close");
 // const container = document.querySelectorAll("container");
 // const card = document.querySelectorAll(".card");
 
@@ -21,22 +21,41 @@ for (let i = 0; i < close.length; i++) {
     //cara non-DOM Transversal
     // card[i].parentElement.removeChild(card[i]); //cara sendiri
     // card[i].style.display = "none"; //cara pak sandhika
-
+    
     // close[i].parentElement.style.display = "none";
-
+    
     e.target.parentElement.style.display = "none";
   });
 }*/
 
+/*
 //Menggunakan forEach
 close.forEach((el) => {
   el.addEventListener("click", (e) => {
     //menelusuri mentransversal element
     e.target.parentElement.style.display = "none";
     e.preventDefault();
+    e.stopPropagation(); //untuk menghentikan event bubbling
+  });
+});
+
+const cards = document.querySelectorAll(".card");
+cards.forEach((card, index) => {
+  card.addEventListener("click", () => {
+    alert("ini card ke-" + (index + 1));
   });
 });
 
 // const nama = document.querySelector(".nama");
 // console.log(nama.parentElement.parentElement);
 // console.log(nama.nextElementSibling.nextElementSibling);
+*/
+
+const container = document.querySelector(".container");
+
+container.addEventListener("click", (e) => {
+  if (e.target.className == "close") {
+    e.target.parentElement.style.display = "none";
+    e.preventDefault(); //buat jaga-jaga kalau yang di klik link
+  }
+});
