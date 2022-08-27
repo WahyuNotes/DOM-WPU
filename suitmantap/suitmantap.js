3eb2335126f7d2b13f60c8fa4d49ef2a112882d6
wramadhan <wahyuramadhan0549@gmail.com>
2022-08-27 09:22:08 +0700

    pembaruan nama

--- suitmantap/suitmantap.js
+++ suitmantap/suitmantap.js
@@ -2,12 +2,15 @@ const komputer = document.querySelector(".komputer");
 const tombol = document.querySelectorAll("button");
 const jumlah = document.querySelector(".jumlah");
 const nyerah = document.querySelector("h3");
+const judul = document.querySelector("h1");
 var counter = 1;
-
+const nama = prompt("masukkan nama anda");
+alert("selamat datang " + nama);
+judul.innerHTML = `Welkom du de hell ${nama}, yu kan bit mi hahaha....`;
 tombol.forEach(function (el) {
   el.addEventListener("click", function (e) {
     if (counter >= 4) {
-      nyerah.innerHTML = `Yang bener kenapa mencetnya. Tangannya bau kali nih, mending cuci kaki, cuci tangan trus tidur, coba lagi dah sampe menang`;
+      nyerah.innerHTML = `Yang bener kenapa mencetnya. ${nama} tangannya bau kali nih, mending cuci kaki, cuci tangan dlu trus coba lagi dah sampe menang`;
       nyerah.style.color = "skyblue";
     }
     if (counter >= 6) {
@@ -15,7 +18,7 @@ tombol.forEach(function (el) {
       nyerah.style.color = "yellow";
     }
     if (counter >= 7) {
-      nyerah.innerHTML = `Main suit aja gak berbakat, mending cuci kaki, cuci tangan trus tidur`;
+      nyerah.innerHTML = `Main suit aja ${nama} gak berbakat, mending cuci kaki, cuci tangan trus tidur`;
       nyerah.style.color = "red";
     }
     if (e.target.classList[0] == "gunting") {
